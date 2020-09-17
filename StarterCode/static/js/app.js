6602421c8eac3b53c9883b10aaf80cec82a284ad
solivas89 <sal.olivas1989@gmail.com>
2020-09-16 20:03:05 -0700

    09/16/20 - Table w/ Data Commit

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// select the table & body from indexed.html
+var ufoTable = d3.select('table');
+
+var tbody = d3.select('tbody');
+
+// Day 2 - Act. #7 & #8 / Day 3 Act. #3
+// iterating throgh each row of each json object in data
+tableData.forEach((ufo) => {
+    // console.log(ufo);
+
+    // append one table row for each sighting in data
+    var row = tbody.append('tr');
+
+    // console log each sighting value
+    Object.entries(ufo).forEach(([key,value]) =>{
+        // console.log(key,value);
+
+        // append a cell to the row for each value 
+        var cell = row.append('td').text(value);
+    })
+})
+
+
+
